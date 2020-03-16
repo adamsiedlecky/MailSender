00eab24ce4eebd67959edce8d3d8d871424d7701
adam <adamsiedlec@wp.pl>
2020-03-16 18:56:01 +0100

    I have cleaned Main class.

--- src/main/java/mailsender/Main.java
+++ src/main/java/mailsender/Main.java
@@ -15,22 +15,9 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
-//        Email email = new Email();
-//
-//        email.setContent("");
-//        email.setSubject("Adam Siedlecki");
-//        email.setReceiverEmail("asiedlecki12@wp.pl");
-//        email.setSenderEmail("asiedlecki12@wp.pl");
-//        email.setSenderPassword("");
-//        email.setSenderPassword("qwerty123");
-//        email.setServerHost("smtp.wp.pl");
-//        email.setServerPort(465);
-//
-//
-//
-//        for (int i = 0; i <50 ; i++) {
-//            Sender.send(email);
-//        }
+
+        // scheduler provided via utils/scheduler/EmailScheduler.java
+        // UI is active on localhost:80
     }
 }
 
